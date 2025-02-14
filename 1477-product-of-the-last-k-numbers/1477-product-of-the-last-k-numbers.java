b78ea4bc41712d45af7e237d1fbb4be8bb90d27b
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-02-14 21:57:59 +0530

    Time: 15 ms (91.34%), Space: 68.8 MB (74.65%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
@@ -1,24 +1,34 @@
- class ProductOfNumbers {
-        ArrayList<Integer>s = new ArrayList<>();
-
-        public ProductOfNumbers() {
-            ArrayList<Integer >s = new ArrayList<>();
-        }
-
-        public void add(int num) {
-            s.add(num);
-        }
-
-        public int getProduct(int k) {
-            
- int m = s.size()-1;
-            int product = 1;
-            while(k!=0){
-                int a = s.get(m);
-                product*=a;
-                k--;
-                m--;
-            }
-            return product;
-        }
+class ProductOfNumbers {
+    List<Integer> list;
+    int prod;
+    public ProductOfNumbers() {
+        list = new ArrayList<>();
+        list.add(1);
+        prod = 1;
     }
+
+    public void add(int num) {
+        if (num == 0) {
+            list.clear();
+            list.add(1);
+            prod = 1;
+            return;
+        }
+        prod *= num;
+        list.add(prod);
+
+    }
+
+    public int getProduct(int k) {
+
+        int end = list.size() - 1;
+        int res = list.get(end);
+         if (k >= list.size()) { 
+            return 0;
+        }
+        if (list.size() == k) {
+            return res;
+        }
+        return res/list.get(end-k);
+    }
+}
