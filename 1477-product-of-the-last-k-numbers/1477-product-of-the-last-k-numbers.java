76c4c6825196021738afaf342b24b96f10213b4e
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-02-14 21:16:46 +0530

    Time: 2685 ms (5.04%), Space: 68.6 MB (83.15%) - LeetHub

---
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
@@ -0,0 +1,24 @@
+ class ProductOfNumbers {
+        List<Integer>s = new ArrayList<>();
+
+        public ProductOfNumbers() {
+            List<Integer >s = new ArrayList<>();
+        }
+
+        public void add(int num) {
+            s.add(num);
+        }
+
+        public int getProduct(int k) {
+            
+ int m = s.size()-1;
+            int product = 1;
+            while(k!=0){
+                int a = s.get(m);
+                product*=a;
+                k--;
+                m--;
+            }
+            return product;
+        }
+    }
