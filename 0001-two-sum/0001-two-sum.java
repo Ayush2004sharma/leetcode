c6e2e40d73e6a6b70974b16d782dfd201d32deba
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-05-18 11:59:41 +0530

    Time: 33 ms (46.27%), Space: 44.9 MB (63.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,34 +1,15 @@
-import java.util.*;
-
 class Solution {
-    public static int[] twoSum(int array[], int key) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == key) {
-                    return new int[]{i, j};
+    public int[] twoSum(int[] nums, int target) {
+        int result[] = new int[2];
+        HashMap<Integer,Integer>hm = new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            for(int j =i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    result[0]=i;
+                    result[1]=j;
                 }
             }
         }
-        throw new IllegalArgumentException("No two elements add up to the key.");
-    }
-
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the number of elements: ");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.print("Enter the target key: ");
-        int key = sc.nextInt();
-
-        int result[] = twoSum(arr, key); 
-        System.out.println("Indices: " + result[0] + ", " + result[1]);
-        
-        sc.close();
+        return result;
     }
 }
