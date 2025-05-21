4ce4247bf90a42a6d0b2ae8c6bf6959ff5756dd9
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-05-21 10:25:57 +0530

    Time: 0 ms (100%), Space: 44.2 MB (85.64%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,19 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    private int diff=Integer.MIN_VALUE;
     public boolean isBalanced(TreeNode root) {
-        return checkBalance(root) != -1;
+        func(root);
+        return diff<2;
     }
 
-    private int checkBalance(TreeNode node) {
-        if (node == null) return 0;
-
-        int left = checkBalance(node.left);
-        if (left == -1) return -1;
-
-        int right = checkBalance(node.right);
-        if (right == -1) return -1;
-
-        if (Math.abs(left - right) > 1) return -1;
-
-        return 1 + Math.max(left, right);
+    int func(TreeNode root){
+        if(root==null)return 0;
+        int lh = func(root.left);
+        int rh = func(root.right);
+        diff= Math.max(diff,Math.abs(lh-rh));
+        return 1+Math.max(lh,rh);
+        
     }
 }
