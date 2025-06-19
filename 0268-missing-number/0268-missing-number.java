dbea48833eb0c194391eb40fe6bcd83d2ba913ba
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-06-19 18:41:05 +0530

    Time: 0 ms (100%), Space: 45.4 MB (43.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int  i =0;
-        for(int n :nums){
-            if(n!=i)break;
-            i++;
+        int sum = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            sum += i;
         }
-        return i ;
+        for (int i : nums) {
+            sum -= i;
+        }
+        return sum;
     }
 }
