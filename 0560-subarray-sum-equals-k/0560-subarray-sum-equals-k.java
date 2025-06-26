8bbb06e0e2025e86d25a742a20ecb2f2d1dbb862
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-06-26 11:26:20 +0530

    Time: 23 ms (92.69%), Space: 47.2 MB (10.96%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,20 +1,20 @@
+import java.util.HashMap;
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        hm.put(0, 1);  // To handle exact match from the beginning
-
-        int sum = 0;
-        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0, prefixSum = 0;
+        
+        map.put(0, 1);
 
         for (int num : nums) {
-            sum += num;
+            prefixSum += num;
 
-            int remaining = sum - k;
-            if (hm.containsKey(remaining)) {
-                count += hm.get(remaining); // Add all the ways this can happen
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
             }
 
-            hm.put(sum, hm.getOrDefault(sum, 0) + 1); // Track prefix sum frequency
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
 
         return count;
