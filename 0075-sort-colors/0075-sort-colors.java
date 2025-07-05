363ea251d3d882054c2bc4297adbf1dc5789130c
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-07-05 18:15:56 +0530

    Time: 0 ms (100%), Space: 41.7 MB (96.98%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,44 +1,24 @@
 class Solution {
-
-     public static void mergeSort(int[] arr, int left, int right) {
-        if (left >= right) return;
-
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
+    public void swap(int arr[], int m , int k){
+        int temp=arr[m];
+        arr[m]=arr[k];
+        arr[k]= temp;
     }
-
-    public static void merge(int[] arr, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
-        int i = left;
-        int j = mid + 1;
-        int k = 0;
-
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                temp[k++] = arr[i++];
-            } else {
-                temp[k++] = arr[j++];
+    public void sortColors(int[] nums) {
+        int l=0;
+        int m=0;
+        int h=nums.length-1;
+        while(m<=h){
+            if(nums[m]==0){
+                swap(nums,l,m);
+                l++;
+                m++;
+            }
+            else if(nums[m]==1)m++;
+            else{
+                swap(nums,m,h);
+                h--;
             }
         }
-
-        while (i <= mid) {
-            temp[k++] = arr[i++];
-        }
-
-        while (j <= right) {
-            temp[k++] = arr[j++];
-        }
-
-        for (int p = 0; p < temp.length; p++) {
-            arr[left + p] = temp[p];
-        }
-    }
-
-    public static  void sortColors(int[] nums) {
-        mergeSort(nums,0,nums.length-1);
     }
 }
