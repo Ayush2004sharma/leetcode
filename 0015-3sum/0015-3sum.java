0df2f39d971a06d0ef976c6c1524a0f764bd7908
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-07-15 12:11:39 +0530

    Time: 2495 ms (5%), Space: 53.6 MB (8.99%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,21 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> s = new ArrayList<>();
-        Arrays.sort(nums);
+        Set<List<Integer>> res = new HashSet<>();
+        int n = nums.length;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int j = i + 1, k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while (j < k && nums[j] == nums[j + 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k - 1])
-                        k--;
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    k--;
+        for (int i = 0; i < n; i++) {
+            Set<Integer> seen = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                int third = - (nums[i] + nums[j]);
+                if (seen.contains(third)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], third);
+                    Collections.sort(triplet); // so [a, b, c] and [b, a, c] are same
+                    res.add(triplet);
                 }
+                seen.add(nums[j]);
             }
         }
-        return s;
+
+        return new ArrayList<>(res);
     }
 }
