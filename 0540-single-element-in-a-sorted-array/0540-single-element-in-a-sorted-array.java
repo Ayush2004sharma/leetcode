9b0df814bf11196847c1d12c3283188ae06b5a85
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-07-18 13:11:46 +0530

    Time: 0 ms (100%), Space: 50.4 MB (40.76%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,9 +3,9 @@ class Solution {
         int left = 0, right = arr.length - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (mid % 2 == 1) mid--; // Ensure mid is even
-            if (arr[mid] == arr[mid + 1]) left = mid + 2; // Move right
-            else right = mid; // Move left
+            if (mid % 2 == 1) mid--; 
+            if (arr[mid] == arr[mid + 1]) left = mid + 2; 
+            else right = mid;
         }
         return arr[left];
     }
