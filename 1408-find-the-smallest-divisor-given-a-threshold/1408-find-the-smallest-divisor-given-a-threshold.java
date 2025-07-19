8a1153c2b36f4e3c548e64afaf95467704e094bc
Ayush sharma <140502261+Ayush2004sharma@users.noreply.github.com>
2025-07-19 13:52:09 +0530

    Time: 13 ms (55.27%), Space: 48.2 MB (51.84%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -1,40 +1,38 @@
 class Solution {
-
     public int smallestDivisor(int[] nums, int threshold) {
-     int left =1;
-     int right =max(nums);  
-     int min =right;
-     while(left<=right){
-        int mid = left+(right-left)/2;
-       int sum=check(nums,mid);
-       if(sum<=threshold){
-        if(min>=mid){
-            min = mid;
+        int max=Integer.MIN_VALUE;
+        
+        for(int i=0; i<nums.length; i++){
+             max=Math.max(max, nums[i]);
         }
-        right=mid-1;
-       }
-       else {
-        left=mid+1;
-       }
-       
-     }
-     return min;
-    }
-    public int max(int nums[]){
-        int max=nums[0];
-        for(int n :nums){
-            if(max<n){
-                max=n;
+        
+        int start=1;
+        int end=max;
+        int ans=Integer.MAX_VALUE;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            long sum=0;
+            for(int i=0; i<nums.length; i++){
+                if(nums[i]<mid){
+                    sum++;
+                }else if(nums[i]>=mid){
+                    if(nums[i]%mid==0){
+                        sum+=nums[i]/mid;
+                    }
+                    else{
+                        sum+=nums[i]/mid+1;
+                    }
+                }
+            }
+
+            if(sum<=threshold){
+                ans=Math.min(ans, mid);
+                end=mid-1;
+            }else{
+                start=mid+1;
             }
         }
-        return max;
+        return ans;
     }
-public int check(int nums[], int k){
-    int sum = 0;
-    for(int i = 0; i < nums.length; i++){
-        sum += (nums[i] + k - 1) / k; // this is equivalent to ceil(nums[i] / k)
-    }
-    return sum;
-}
-
 }
