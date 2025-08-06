63823b3a1eea4add0f944a652c7906dca191e68a
Ayush sharma <ayushsharma1153@gmail.com>
2025-08-06 15:10:08 +0530

    Time: 1 ms (100%), Space: 44.6 MB (46.44%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.java
@@ -1,21 +1,18 @@
 class Solution {
-
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        ArrayList<Integer>l= new ArrayList<>();
-         for(int j:baskets){
-            l.add(j);
-         }
- 
-         int count =fruits.length;
-         for(int n:fruits){
-         for(int  j=0;j<l.size();j++){
-            if(n<=l.get(j)){
-                count--;
-                l.remove(j);
-                break;
+        int n = fruits.length,alloted=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(fruits[i]<=baskets[j])
+                {
+                    alloted++;
+                    baskets[j] = -1;
+                    break;
+                }
             }
-         }
-         }
-         return count;
+        }
+        return n - alloted;
     }
 }
