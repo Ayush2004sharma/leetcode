88662c2b43e3860313ec0ea17484c7f81ccbf4dd
Ayush sharma <ayushsharma1153@gmail.com>
2025-09-06 17:32:17 +0530

    Time: 1 ms (99.84%), Space: 46.3 MB (36.11%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
         for (int num : nums) {
-            result ^= num; // XOR cancels duplicates
+            result ^= num; 
         }
         return result;
     }
