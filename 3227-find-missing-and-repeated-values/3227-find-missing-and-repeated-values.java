1c3825e65f637a74cc2c2765b270272cd5278ce3
Ayush sharma <ayushsharma1153@gmail.com>
2025-09-06 17:04:13 +0530

    Time: 5 ms (40.46%), Space: 45.8 MB (14.8%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,27 +1,23 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int []ans = new int [2];
+        int[] ans = new int[2];
         int n = grid.length;
-        HashSet<Integer>hm = new HashSet<>();
-        boolean []vis= new boolean [(n*n)+1];
-        for ( int i =0;i<n;i++){
-            for( int j=0;j<n;j++){
-                if( !vis[grid[i][j]]){
-                    vis[grid[i][j]]=true;
-                }
-                if(hm.contains(grid[i][j])){
-                    ans[0]=grid[i][j];
-                }
-                else{
+        int sum =0;
+        for(int i=1;i<=n*n;i++){
+            sum+=i;
+        }
+        HashSet<Integer> hm = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (hm.contains(grid[i][j])) {
+                    ans[0] = grid[i][j];
+                } else {
                     hm.add(grid[i][j]);
+                    sum-=grid[i][j];
                 }
             }
         }
-        for ( int i =0;i<vis.length;i++){
-            if( !vis[i]){
-                ans[1]=i;
-            }
-        }
-        return ans ;
+        ans[1]=sum;
+        return ans;
     }
 }
