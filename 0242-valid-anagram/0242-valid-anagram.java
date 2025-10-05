35d77d2f7a3eff2a20eb2b38ec1eecd77bc7d2c6
Ayush sharma <ayushsharma1153@gmail.com>
2025-10-05 13:28:13 +0530

    Time: 25 ms (6.36%), Space: 44.5 MB (54.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,15 @@
-import java.util.HashMap;
-
 public class Solution {
-    public static boolean isAnagram(String s, String t) {
+    public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
         }
 
-        HashMap<Character, Integer> freqMap = new HashMap<>();
-
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
         }
-
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            if (!freqMap.containsKey(c)) {
-                return false;
-            }
-            freqMap.put(c, freqMap.get(c) - 1);
-            if (freqMap.get(c) == 0) {
-                freqMap.remove(c);
-            }
-        }
-
-        return freqMap.isEmpty();
+        return countS.equals(countT);
     }
 }
